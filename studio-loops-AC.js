295dd5272e5ab54b52fe089c40a3525425e05f13
Shandy Casteel <shandycasteel@gmail.com>
2020-01-25 20:04:22 -0600

    Start Loops Studio

---
+++ studio-loops-AC.js
@@ -0,0 +1,51 @@
+//Part A #1: Initialize variables to store the following arrays. Remember to use descriptive names.
+
+let proteinOptions = ['chicken', 'pork', 'tofu', 'beef', 'fish', 'beans'];
+let grainOptions = ['rice', 'pasta', 'corn', 'potato', 'quinoa', 'crackers'];
+let vegetableOptions = ['peas', 'green beans', 'kale','edamame', 'broccoli', 'asparagus'];
+let beverageOptions = ['juice', 'milk', 'water', 'soy milk', 'soda', 'tea'];
+let dessertOptions = ['apple', 'banana', 'more kale','ice cream', 'chocolate', 'kiwi'];
+
+//Part A #2: Construct a for loop that assembles a meal for each of 6 astronauts.
+
+let meal = [];
+
+for (i = 0; i < 6; i++) {
+  meal[i] = [
+    proteinOptions[i],
+    grainOptions[i],
+    vegetableOptions[i],
+    beverageOptions[i],
+    dessertOptions[i]
+  ];
+}
+
+for (i = 0; i < meal.length; i++) {
+  console.log(meal[i]);
+  console.log("-----");
+}
+
+
+//Part C #5: Using a while loop, ask the user to select the number of meals to assemble.  Validate the input to make sure it is an integer from 1 - 6.
+
+let numberOfMeals = 0;
+
+const input = require('readline-sync');
+
+numberOfMeals = Number(input.question("Enter the number of meals (1-6) you'd like assembled: "));
+
+while (numberOfMeals < 1 || numberOfMeals > 6) {
+  numberOfMeals = Number(input.question("That's not a valid option. Please enter the number of meals you'd like assembled: "));
+}
+
+for (i = 0; i < numberOfMeals; i++) {
+  console.log(meal[i]);
+}
+
+
+
+
+//Skill boosts: 
+// a. Use string formatting to print something more interesting than “[‘chicken’, ‘rice’, ‘peas’, ‘juice’, ‘apple’]” for the meal outputs.
+// b. Use an “array of arrays” to store the food options in a ‘pantry’.
+// c. Modify your code to check each meal for kale.  If present, after the meal output add, “Don’t worry, you can have double chocolate tomorrow.”
---
+++ studio-loops-B.js
@@ -0,0 +1,16 @@
+//Construct a for loop that combines two strings together, alternating the characters from each source.  Print the result.
+
+//Example: If string = “1234” and otherString = “5678”, then the output will be “15263748”.
+
+//For now, make sure the two strings contain the same number of characters!
+
+let strOne = 'Rutabagas';
+let strTwo = 'Chemistry';
+let strNew = '';
+
+for (i = 0; i < strOne.length; i++) {
+    strNew = strNew + strOne[i];
+    strNew = strNew + strTwo[i];
+}
+
+console.log(strNew);
